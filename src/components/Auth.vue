3004b83b6f3747923383bf6ccbf1b83802e75eeb
DanielMa <mwldaniel_008@126.com>
2022-11-09 11:58:13 +0800

    Custom Error Messages && Showing alert

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -66,8 +66,9 @@
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input
+              <vee-field
                 type="email"
+                name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
@@ -75,8 +76,9 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input
+              <vee-field
                 type="password"
+                name="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
@@ -89,10 +91,18 @@
             </button>
           </vee-form>
           <!-- Registration Form -->
+          <div
+            class="text-white text-center font-bold p-4 rounded mb-4"
+            v-if="reg_show_alert"
+            :class="reg_alert_variant"
+          >
+            {{ reg_alert_msg }}
+          </div>
           <vee-form
             v-show="tab === 'register'"
             :validation-schema="schema"
             @submit="register"
+            :initial-values="userData"
           >
             <!-- Name -->
             <div class="mb-3">
@@ -184,6 +194,7 @@
             </div>
             <button
               type="submit"
+              :disabled="reg_in_submission"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
             >
               Submit
@@ -208,10 +219,17 @@ export default {
         email: "required|min:3|max:100|email",
         age: "required|min_value:18|max_value:130",
         password: "required|min:9|max:100|excluded:password",
-        confirm_password: "confirmed:@password",
-        country: "required|excluded:Antarctica",
-        tos: "required",
+        confirm_password: "passwords_mismatch:@password",
+        country: "required|country_excluded:Antarctica",
+        tos: "tos",
       },
+      userData: {
+        country: "USA",
+      },
+      reg_in_submission: false,
+      reg_show_alert: false,
+      reg_alert_variant: "bg-blue-500",
+      reg_alert_msg: "Please wait! your account is being created.",
     };
   },
   computed: {
@@ -222,6 +240,13 @@ export default {
   },
   methods: {
     register(values) {
+      this.reg_show_alert = true;
+      this.reg_in_submission = true;
+      this.reg_alert_variant = "bg-blue-500";
+      this.reg_alert_msg = "Please wait! your account is being created.";
+
+      this.reg_alert_variant = "bg-green-500";
+      this.reg_alert_msg = "Success! Your account has been created.";
       console.log(values);
     },
   },
