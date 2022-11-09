62fe08dc3eb8747562640ad5c7ab256f1a5c8150
DanielMa <mwldaniel_008@126.com>
2022-11-09 12:19:33 +0800

    Setting up the Login Form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -62,7 +62,11 @@
           </ul>
 
           <!-- Login Form -->
-          <vee-form v-show="tab === 'login'">
+          <vee-form
+            v-show="tab === 'login'"
+            :validation-schema="loginSchema"
+            @submit="login"
+          >
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -72,6 +76,7 @@
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email"
               />
+              <ErrorMessage class="text-red-600" name="email" />
             </div>
             <!-- Password -->
             <div class="mb-3">
@@ -82,6 +87,7 @@
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password"
               />
+              <ErrorMessage class="text-red-600" name="password" />
             </div>
             <button
               type="submit"
@@ -230,6 +236,10 @@ export default {
       reg_show_alert: false,
       reg_alert_variant: "bg-blue-500",
       reg_alert_msg: "Please wait! your account is being created.",
+      loginSchema: {
+        email: "required|email",
+        password: "required|min:9|max:100",
+      },
     };
   },
   computed: {
@@ -247,6 +257,9 @@ export default {
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
+      console.log(values);
+    },
+    login(values) {
       console.log(values);
     },
   },
