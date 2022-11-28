92b23e8b08c271333128cd69b4778065d469020b
DanielMa <mwldaniel_008@126.com>
2022-11-28 23:45:53 +0800

    Storing the list of songs and display them

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -111,6 +111,16 @@ export default {
         );
       });
     },
+    // cancelUploads() {
+    //   this.uploads.forEach((upload) => {
+    //     upload.task.cancel();
+    //   });
+    // },
+  },
+  beforeUnmount() {
+    this.uploads.forEach((upload) => {
+      upload.task.cancel();
+    });
   },
 };
 </script>
